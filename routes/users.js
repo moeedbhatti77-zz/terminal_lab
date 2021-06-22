a4c7411c7d3b25156c5b5b830742dc7a64097345
Moeed <moeedbhatti419@gmail.com>
2021-06-22 14:20:23 +0500

    login - signup done

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const Users = require("../models/UsersModel");
+
 /* GET users listing. */
 router.get('/signup',async function(req, res, next) {
   res.render('signup');
 });
 router.get('/login',async function(req, res, next) {
   res.render('login');
+});
+router.post('/login',async function(req, res, next) {
+  let user = await Users.findOne({
+    email: req.body.email,
+    password : req.body.password
+  });
+  if(!user)
+  res.redirect('/users/login');
+  
+  res.redirect('/');
 });
 router.post('/signup',async function(req, res, next) {
   user = new Users(req.body);
